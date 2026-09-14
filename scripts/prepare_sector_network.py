ec7a1a30a5ea2cb9e95337d4afcffc2b5c04211e
Yerbol-Akhmetov <yerbol.akhmetov.93@gmail.com>
2026-09-14 17:33:40 +0200

    remove gas generators

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -452,6 +452,10 @@ def add_carrier_buses(
 
             suffix = " primary"
 
+        # omit gas generators
+        if carrier == "gas":
+            return
+
         n.add(
             "Generator",
             nodes + suffix,
