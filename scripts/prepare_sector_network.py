07bcec8a2da4811d1ca19c7541231c76d1946c35
Yerbol-Akhmetov <yerbol.akhmetov.93@gmail.com>
2026-09-16 07:12:10 +0200

    bring gas generators back

--- scripts/prepare_sector_network.py
+++ scripts/prepare_sector_network.py
@@ -453,10 +453,6 @@ def add_carrier_buses(
 
             suffix = " primary"
 
-        # omit gas generators
-        if carrier == "gas":
-            return
-
         n.add(
             "Generator",
             nodes + suffix,
